096f930cd03d73181b7d3c8b893c6b8e010a0c35
Rigo Razo <rigovr7@gmail.com>
2025-02-02 14:27:53 -0500

    First commit

---
+++ Exercise.java
@@ -0,0 +1,65 @@
+public class Exercise {
+    private String name;
+    private int sets;
+    private int reps;
+    private boolean isCompleted;
+
+    public Exercise() {
+        this("unknown");
+    }
+
+    public Exercise(String name) {
+        this(name, 0);
+        this.name = name;
+    }
+
+    public Exercise(String name, int sets) {
+        this(name, sets , 0);
+        this.name = name;
+        this.sets = sets;
+    }
+
+    public Exercise(String name, int sets, int reps) {
+        this.name = name;
+        this.sets = sets;
+        this.reps = reps;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + " + " + this.sets + " + " + this.reps;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Exercise> list = new ArrayList<Exercise>();
+        Exercise rigo = new Exercise();
+
+
+
+    }
+
+    public static void addExercise(String name, int sets, int reps){
+
+    }
+
+    public static void removeExercise( int index){
+
+    }
+
+    public static void updateExercise(int index, String newName, int newSets, int newReps){
+
+    }
+
+    public static void markExerciseAsCompleted(int index){
+
+    }
+
+    public static void displayWorkoutPlan(ArrayList list ){
+        System.out.println(list);
+
+    }
+
+}
